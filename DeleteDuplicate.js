5875065246335172bbd6891eb14d0cc452bcc5b4
Becki Blumer <48719280+beckibloom@users.noreply.github.com>
2019-12-20 11:30:32 -0600

    completed deleteduplicate.js

--- DeleteDuplicate.js
+++ DeleteDuplicate.js
@@ -1,2 +1,25 @@
 // Given a sorted linked list, write an algorithm to delete all duplicate numbers from the sorted linked list.
 
+// Input (linked list): 1 > 2 > 3 > 3 > 4 > 5
+// Output (linked list): 1 > 2 > 3 > 4 > 5
+
+// iterate through the items, and if the value of the current node = node.next, then delete the current node, changing the .next pointer of the prev node to the node.next (skipping current node)
+
+function(list) {
+  let prevNode = null;
+  let currNode = list.head;
+  let nextNode = list.head.next;
+  while (node.next !== null) {
+    if (currNode === nextNode) {
+      if (prevNode = null) {
+        list.head = nextNode;
+      } else {
+        prevNode.next = nextNode
+      }
+    } else {
+      prevNode = currNode;
+      currNode = nextNode;
+      nextNode = nextNode.next;
+    }
+  }
+}
